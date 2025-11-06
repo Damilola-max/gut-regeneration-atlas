dd6821c758c9f241aa2e419bed55924ef91e6433
Damilola Olayemi <79061444+Damilola-max@users.noreply.github.com>
2025-11-06 20:38:16 +0100

    utils_io.R

--- R/utils_io.R
+++ R/utils_io.R
@@ -1,1 +1,7 @@
 # utility script placeholder
+library(yaml)
+cfg <- yaml::read_yaml("config/config.yml")
+
+ensure_dir <- function(path) if (!dir.exists(path)) dir.create(path, recursive = TRUE)
+
+ppath <- function(...) file.path(...)
